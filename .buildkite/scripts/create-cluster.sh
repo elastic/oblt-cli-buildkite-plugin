c54bbb603332828df10e8954fb40d4d560124c32
Jan Calanog <jan.calanog@elastic.co>
2024-07-05 23:00:31 +0200

    fix

--- .buildkite/scripts/create-cluster.sh
+++ .buildkite/scripts/create-cluster.sh
@@ -3,11 +3,13 @@
 set -euo pipefail
 
 echo "--- Create cluster"
+output_file="${PWD}/cluster-info.json"
 oblt-cli cluster create custom \
 	--dry-run \
 	--template serverless \
 	--cluster-name-prefix=oblt-cli-buildkite-plugin \
-	--parameters='{"ProjectType":"observability","Target":"qa","EphemeralCluster":"true"}'
+	--parameters='{"ProjectType":"observability","Target":"qa","EphemeralCluster":"true"}' \
+	--output-file "${output_file}"
 
 echo "~~~ Add cluster name to meta-data"
-buildkite-agent meta-data set cluster-name "$(jq -r .ClusterName "${PWD}/cluster-info.json")"
+buildkite-agent meta-data set cluster-name "$(jq -r .ClusterName "${output_file}")"
