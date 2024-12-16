5941fe56ab61576eecd5e468b8d1c9ec6d14252f
Jan Calanog <jan.calanog@elastic.co>
2024-12-16 13:52:17 +0100

    ok

--- lib/asset.bash
+++ lib/asset.bash
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+readonly ASSET_REPOSITORY_NAME='elastic/observability-test-environments'
+
 # Get the OS name
 # Arguments:
 #   $1: The system name - (optional)
@@ -77,7 +79,7 @@ function get_asset_id() {
 		-H "Accept: application/vnd.github+json" \
 		-H "Authorization: Bearer ${VAULT_GITHUB_TOKEN}" \
 		-H "X-GitHub-Api-Version: 2022-11-28" \
-		"https://api.github.com/repos/elastic/oblt-cli/releases/tags/${version}")
+		"https://api.github.com/repos/${ASSET_REPOSITORY_NAME}/releases/tags/${version}")
 	echo "$release" | jq -r --arg name "$asset_name" '.assets | .[] | select(.name == $name) | .id'
 }
 
@@ -96,5 +98,5 @@ function download_asset() {
 		-H "Accept: application/octet-stream" \
 		-H "Authorization: Bearer ${VAULT_GITHUB_TOKEN}" \
 		-H "X-GitHub-Api-Version: 2022-11-28" \
-		"https://api.github.com/repos/elastic/oblt-cli/releases/assets/${asset_id}" | tar -xz -C "$target_dir"
+		"https://api.github.com/repos/${ASSET_REPOSITORY_NAME}/releases/assets/${asset_id}" | tar -xz -C "$target_dir"
 }
