62924aa6175a9789383a4476e2c2ad37a005cb7c
Victor Martinez <VictorMartinezRubio@gmail.com>
2026-05-11 12:53:03 +0200

    support dry-run optionally

--- .buildkite/scripts/create-cluster.sh
+++ .buildkite/scripts/create-cluster.sh
@@ -3,12 +3,20 @@
 set -euo pipefail
 
 echo "--- Create cluster"
+
+DRY_RUN_FLAG=${1:-""}
+
+DRY_RUN_ARG=""
+if [ "$DRY_RUN_FLAG" = "--dry-run" ]; then
+	DRY_RUN_ARG="--dry-run"
+fi
+
 oblt-cli cluster create custom \
-	--dry-run \
 	--template serverless \
 	--cluster-name-prefix=oblt-cli-buildkite-plugin \
 	--parameters='{"ProjectType":"observability","Target":"qa","EphemeralCluster":"true"}' \
-	--output-file="${PWD}/cluster-info.json"
+	--output-file="${PWD}/cluster-info.json" \
+	$DRY_RUN_ARG
 
 echo "~~~ Add cluster name to meta-data"
 buildkite-agent meta-data set cluster-name "$(jq -r .ClusterName "${PWD}/cluster-info.json")"
